0df3ee8c60276ba55cbc3a4fbbef37d93d665429
German Ramos Garcia <german.ramos.garcia@bbva.com>
2016-05-25 13:37:33 +0200

    fix: syntax

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ echo "Pending..." > ${PWD}/${FILE}
 while true; do
   /bin/sh docker-bench-security.sh | tee -a report.part
   mv report.part ${PWD}/${FILE}
-  echo "Waiting 60 seconds for next report"
+  echo "Waiting ${INTERVAL} seconds for next report"
   sleep ${INTERVAL}
 done

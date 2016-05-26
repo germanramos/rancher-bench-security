e2f5ba55d1e752ff0036b1cb7e8009651b15abc9
German Ramos Garcia <german.ramos.garcia@bbva.com>
2016-05-26 13:13:41 +0200

    feat: Add hostname to report

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ cp web/* ${PWD}
 echo "Pending..." > ${PWD}/${FILE}
 
 while true; do
+  echo "Hostname: `hostname`" > report.part
   /bin/sh docker-bench-security.sh | tee -a report.part
   mv report.part ${PWD}/${FILE}
   echo "Waiting ${INTERVAL} seconds for next report"
